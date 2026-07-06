1eb2b5a0a5dcaf7dcf79c1aedab16a39febc4b4f
DMStyles <dilshanmadushanka8520@gmail.com>
2026-07-06 23:22:49 +0530

    Bump version to 1.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.subscriptionmanager"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.9"
+        versionCode = 9
+        versionName = "1.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
